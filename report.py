3af0a4fda75291b1e680439762fedeb7aeffcd9a
Sowbaranikaa O <165006224+sowbaranikaao@users.noreply.github.com>
2026-07-16 11:39:26 +0530

    Delete report.py

--- report.py
+++
@@ -1,6 +0,0 @@
-employees = ["John", "Alice", "Sam"]
-salary = [50000, 60000, 70000]
-with open("salary.txt","w") as file:
-    for emp,sal in zip(employees,salary):
-        file.write(f"{emp}:{sal}\n")
-print("File created")
