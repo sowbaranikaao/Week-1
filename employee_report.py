6a0e3ec9faf4cb1c83e5138a843b8b34db765e17
Sowbaranikaa O <165006224+sowbaranikaao@users.noreply.github.com>
2026-07-16 11:37:51 +0530

    Delete employee_report.py

--- employee_report.py
+++
@@ -1,6 +0,0 @@
-emp_name=["Alice","Bob","John"]
-salary=[50000,60000,70000]
-with open("report.txt","w") as file:
-    for index,(name,sal) in enumerate(zip(emp_name,salary),start=1):
-        bonus=sal*0.1
-        file.write(f"{index}. {name} Salary: {sal} Bonus: {bonus} Total: {sal+bonus}\n")
